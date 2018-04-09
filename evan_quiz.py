8199844410c8b011ea9d615bb474147b860cdeb9
Evan Fields <efriver01@hotmail.com>
2018-04-09 11:58:25 -0400

    add quiz file

---
+++ evan_quiz.py
@@ -0,0 +1,36 @@
+print("welcome to Evans quiz ")
+
+question1 = input("what color is an apple?")
+awnser1 = ("red")
+
+if question1 == awnser1:
+    print("Correct!!!")
+elif question1 != awnser1:
+    print ("incorrect")
+
+question2 = input("what currency does mario collect Super Mario Bros?")
+awnser2 = ("coins")
+
+if question2 == awnser2:
+    print("Correct!!!")
+elif question2 != awnser2:
+    print("incorrect!")
+
+question3 = input("what is link's favorite horse?")
+awnser3 = ("epona")
+
+if question3 == awnser3:
+    print("Correct!!!")
+elif question3 != awnser3:
+    print("incorrect!")
+
+question4 = input("what color is spyro?")
+awnser4 = ("purple")
+
+if question4 == awnser3:
+    print("Correct!!!")
+    
+    
+
+goodbye = input("thanks for playing! press enter to exit.")
+                  
