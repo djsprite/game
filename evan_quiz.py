f15658a418c5881fb71bb591275c9c72da5771a3
Evan Fields <36670815+djsprite@users.noreply.github.com>
2018-04-20 11:51:41 -0400

    Add files via upload

--- evan_quiz.py
+++ evan_quiz.py
@@ -1,70 +1,85 @@
-print("welcome to Evans quiz ")
+print("welcome to Evans video game quiz")
 print("There are 7 questions good luck on the quiz")
 def end_game(ans):
     if ans == "quit":
         print('Game over')
         # exit
         quit()
-#question1 = super easy
-question1 = input("what color is an apple?")
-awnser1 = ("red")
+score = 0
+correct_answer = True
+def correct(score):
+    print("correct") 
+    score = score + 1
+
+def incorrect(score):
+    print ("incorrect") 
+    score = 0
+    print(score)
+    #score_total = 7
+    
+question1 = input("what is the name of the red ghost in pacman?")
+awnser1 = "blinky"
 end_game(question1)
 if question1 == awnser1:
-    print("Correct!!!")
+    correct(score)
 else:
-    print ("incorrect")
+    incorrect(score)
 
 question2 = input("what currency does Mario collect in Super Mario Bros?")
 awnser2 = ("coins")
 end_game(question2)
 
 if question2 == awnser2:
-    print("Correct!!!")
+    print("correct")
 else:
-    print("incorrect!")
+    print("incorrect")
 
 question3 = input("what is link's favorite horse?")
 awnser3 = ("epona")
 end_game(question3)
 
 if question3.lower().strip() == awnser3:
-    print("Correct!!!")
+    print("correct")
 else:
-    print("incorrect!")
+    print("incorrect")
 
 question4 = input("what color is spyro?")
 answer4 = "purple"
 end_game(question4)
 
 if question4 == answer4:
-    print("Correct!!!")
+    print("correct")
 else:
     end_game(question4)
-    print("incorrect!")
+    print("incorrect")
 
-question5 = input("what is sonics archnemesis?")
-answer5 = ("dr eggman")
+question5 = input("what is the name of sonics sidekick?")
+answer5 = ("tails")
 end_game(question5)
 
 if question5 == answer5:
-    print("Correct!!!")
+    print("correct")
 else:
-    print("incorrect!")
+    print("incorrect")
 
-question6 = input("how does kirby copy his enemies moves")
-answer6 = ("he inhales them")
+question6 = input("what was the nes called in japan?")
+answer6 = ("the famicom")
 end_game(question6)
 
 if question6 == answer6:
-    print("Correct!!!")
+    print("correct")
 else:
-    print("incorrect!")
+    print("incorrect")
 
 question7 = input("who is the father of video games")
 answer7 = ("ralph baer")
 end_game(question7)
 
 if question7 == answer7:
-    print("Correct!!!")
+    print("correct")
 else:
-    print("incorrect!")
+    print("incorrect")
+    print("thanks for playing")
+    
+    
+    
